383d7077fe603ab49f662409dc6e48564dfa398a
Jay Almers <jaya@arcatechsystems.com>
2016-06-01 10:06:31 -0400

    Initial commit

---
+++ connect.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+import "time"
+
+// Define connection constants
+const ORG_ID 	  string = "gj2fv7"
+const DEV_TYPE 	  string = "ARCA-Go-Device-1"
+const DEV_ID 	  string = "mebane-nc-cm18"
+const AUTH_METHOD string = "token"
+const AUTH_TOKEN  string = "UaqWS4cWMdaNBpVWU6"
+
+func main() {
+    fmt.Println(time.Now())
+}
