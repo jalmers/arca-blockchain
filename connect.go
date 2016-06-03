2835197d3d73f094d593fc35da80a68c863e5d98
Jay Almers <jaya@arcatechsystems.com>
2016-06-03 14:53:08 -0400

    Fixed issue with extraneous code

--- connect.go
+++
@@ -1,6 +0,0 @@
-// Define connection constants
-const ORG_ID 	  string = "gj2fv7"
-const DEV_TYPE 	  string = "ARCA-Go-Device-1"
-const DEV_ID 	  string = "mebane-nc-cm18"
-const AUTH_METHOD string = "token"
-const AUTH_TOKEN  string = "UaqWS4cWMdaNBpVWU6"
